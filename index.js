452f27c98793d719b69c79fe29e0b7f76d7ced52
HURNAJO <jose.leo.hurna@oocl.com>
2020-09-30 18:56:28 +0800

    String Exercise Javascript

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
-var name = 'hello';
-// TODO write your code here...
-
+var name = "hello";
+var nameCaps = name.toUpperCase();
+console.log(nameCaps);
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
-var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
+var sentence = 'Wow afternoon, mr mike.';
+function titleCase(sentence) {
+    var splitStr = sentence.toLowerCase().split(' ');
+    for (var i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    return splitStr.join(' '); 
+ }
+ console.log(titleCase(sentence));
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
-//TODO write your code here...
+var extracted = money.substring(1);
+console.log(extracted);
